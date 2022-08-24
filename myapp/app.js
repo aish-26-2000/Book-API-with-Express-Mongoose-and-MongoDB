0b16ec131f26195af0c3efa38e8a6055f88a0baf
aish-26-2000 <sreedeviaishu41@gmail.com>
2022-08-25 00:28:52 +0530

    User Model and User Signup updated

--- myapp/app.js
+++ myapp/app.js
@@ -45,6 +45,7 @@ app.use((req,res,next)=>{
 });
 
 app.use('/books',bookRouter)
+app.use('/users',userRouter)
 
 app.all('*',(req,res,next)=>{
     next(new AppError(`Can't find ${req.originalUrl} on this server.`,404));
---
+++ myapp/models/userModel.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+//Creating Schema
+const userSchema = new mongoose.Schema({
+    name :{
+        type : String,
+        required : [true, 'User must have a name.'],
+    },
+    email :{
+        type : String,
+        required : [true, 'User must have an email.'],
+        unique : true,
+        lowercase : true,
+        validate : [validator.isEmail, 'Provide a valid email.']
+    },
+    password :{
+        type : String,
+        required : [true, 'User must have a password.'],
+        minlength : 8
+    },
+    passwordConfirm :{
+        type : String,
+        required : [true, 'Confirm the password'],
+        validate : {
+            //works on save
+            validator : function(el) {
+                return el === this.password; //abc === abc
+            },
+        message : "Passwords are not the same"   
+        } 
+    },
+});
+
+userSchema.pre('save',async function(next){
+    //run function if password is modified
+    if (!this.isModified('password')) return next();
+
+    //hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+
+    //delete passwordConfirm field
+    this.passwordConfirm = undefined;
+    next();
+});
+
+const User = mongoose.model('User',userSchema);
+
+module.exports = User;
